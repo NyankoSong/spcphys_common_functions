3663b73a27660711f1212b68bebb712bda052f65
NyankoSong <2010220018@email.cugb.edu.cn>
2024-10-30 16:07:09 +0800

    modified:   src/spcphys_common_functions/utils/net_tools.py 	modified:   tests/cdf_process_test.ipynb

--- src/spcphys_common_functions/utils/net_tools.py
+++ src/spcphys_common_functions/utils/net_tools.py
@@ -4,7 +4,6 @@ import warnings
 from typing import List, Tuple
 from datetime import datetime, timedelta
 import asyncio
-semaphore = asyncio.Semaphore(5) 
 import nest_asyncio
 nest_asyncio.apply()
 import aiohttp
@@ -18,39 +17,64 @@ from . import config
 from .utils import check_parameters
 
 
-async def _fetch_file(dataset, varname, start_time, chunk_end_time, save_path):
+async def _fetch_file(semaphore, dataset, varname, start_time, chunk_end_time, save_path, retries, delay):
     async with semaphore:
         loop = asyncio.get_event_loop()
-        http_status, results = await loop.run_in_executor(None, cdas.get_data_file, dataset, varname, start_time, chunk_end_time)
-        if http_status == 200:
-            url = results['FileDescription'][0]['Name']
-            filename = url.split('/')[-1]
+        for attempt in range(1, retries + 1):
             try:
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(url) as response:
-                        if response.status == 200:
-                            with open(os.path.join(save_path, filename), 'wb') as f:
-                                while True:
-                                    chunk = await response.content.read(1024*1024)
-                                    if not chunk:
-                                        break
-                                    f.write(chunk)
-                        else:
-                            warnings.warn(f'Failed to fetch {filename}: HTTP {response.status}')
-                print(f'{filename} saved to {save_path}')
+                http_status, results = await loop.run_in_executor(None, cdas.get_data_file, dataset, varname, start_time, chunk_end_time)
+                if http_status == 200:
+                    url = results['FileDescription'][0]['Name']
+                    filename = url.split('/')[-1]
+                    try:
+                        async with aiohttp.ClientSession() as session:
+                            async with session.get(url) as response:
+                                if response.status == 200:
+                                    with open(os.path.join(save_path, filename), 'wb') as f:
+                                        while True:
+                                            chunk = await response.content.read(1024*1024)
+                                            if not chunk:
+                                                break
+                                            f.write(chunk)
+                                    # print(f'{filename} saved to {save_path}')
+                                    return  # 成功，退出函数
+                                else:
+                                    warnings.warn(f'Failed to fetch {filename}: HTTP {response.status}')
+                    except Exception as e:
+                        warnings.warn(f'Error occurred while fetching {filename}: {str(e)}')
+                else:
+                    warnings.warn(f'Failed to fetch {dataset}: HTTP {http_status}')
             except Exception as e:
-                warnings.warn(f'Error occured while fetching {filename}: {str(e)}')
-        else:
-            warnings.warn(f'Failed to fetch {dataset}: HTTP {http_status}')
+                warnings.warn(f'Attempt {attempt} failed with error: {str(e)}')
+            if attempt < retries:
+                await asyncio.sleep(delay)
+                warnings.warn(f'Retrying to fetch {filename} ({attempt}/{retries})...')
+        warnings.warn(f'Failed to fetch {dataset} after {retries} attempts')
         
         
-async def _fetch_files(file_info_list):
-    tasks = [_fetch_file(dataset, varname, start_time, chunk_end_time, save_path) for dataset, varname, start_time, chunk_end_time, save_path in file_info_list]
+async def _fetch_files(semaphore, file_info_list, retries, delay):
+    tasks = [_fetch_file(semaphore, dataset, varname, start_time, chunk_end_time, save_path, retries, delay) for dataset, varname, start_time, chunk_end_time, save_path in file_info_list]
     for task in tqdm.as_completed(tasks, total=len(tasks), desc="Downloading"):
         await task
 
+@check_parameters
+def fetch_cdf_from_cdaweb(cdf_info: dict|str, time_range: List[datetime]|Tuple[datetime], time_chunk: timedelta =timedelta(days=30), save_path: str|dict|None =None, info_filename: str|None =None, retries: int =3, delay: int =2, max_concurrent: int =3) -> List[Tuple[str, List[str], datetime, datetime, str]]:
+    """
+    Fetch CDF files from CDAWeb based on provided dataset information and time range.
 
-def fetch_cdf_from_cdaweb(cdf_info: dict|str, time_range: List[datetime]|Tuple[datetime], time_chunk: timedelta =timedelta(days=30), save_path: str|dict|None =None, info_filename: str|None =None, original_cdf_file: bool =False):
+    :param cdf_info: A dictionary or string containing dataset and variable name information.
+                     - If a dictionary, it maps satellite names to their datasets and variable names. It should be in the format {satellite1: {dataset1: [varname1, varname2, ...], dataset2: [...], ...}, satellite2: {...}, ...} or {dataset1: [varname1, varname2, ...], dataset2: [...], ...} if there is no need to differentiate between satellites.
+                     - If a string, it can be a directory path containing satellite info files or a CSV file with dataset and variable names. The CSV file should have columns 'dataset' and 'varname', detailed information can be found in cdf_process.process_satellite_data.
+    :param time_range: A list or tuple of two datetime objects specifying the start and end times for data retrieval.
+    :param time_chunk: Optional; a timedelta object specifying the time chunk size for data retrieval. Defaults to 30 days.
+    :param save_path: Optional; a string or dictionary specifying the path(s) to save the downloaded CDF files.
+                      - If None, defaults to the current working directory.
+                      - If a string, the same path is used for all satellites.
+                      - If a dictionary, it maps satellite names to their respective save paths.
+    :param info_filename: Optional; a string specifying the filename of the info file when `cdf_info` is a directory.
+    
+    :return file_info_list: A list of tuples containing dataset, variable name, start time, end time, and save path for each file.
+    """
     
     DEFAULT_SAVE_DIRNAME = 'cdf_files'
     
@@ -69,9 +93,9 @@ def fetch_cdf_from_cdaweb(cdf_info: dict|str, time_range: List[datetime]|Tuple[d
         elif os.path.isfile(cdf_info):
             satellite_file_info = pd.read_csv(cdf_info)
             cdf_info = {}
-            save_path = {}
             cdf_info[DEFAULT_SAVE_DIRNAME] = {info[0]: info[1].split() for info in satellite_file_info.loc[:, ['dataset', 'varname']].values}
-            save_path[DEFAULT_SAVE_DIRNAME] = os.path.join(os.path.dirname(cdf_info), DEFAULT_SAVE_DIRNAME)
+            if save_path is None:
+                save_path = os.path.dirname(cdf_info)
             
     if isinstance(cdf_info, dict) and all(isinstance(value, list) for value in cdf_info.values()):
         cdf_info = {DEFAULT_SAVE_DIRNAME: cdf_info}
@@ -87,20 +111,19 @@ def fetch_cdf_from_cdaweb(cdf_info: dict|str, time_range: List[datetime]|Tuple[d
             
     
     file_info_list = []
-    if not original_cdf_file:
-        for satelite, info in cdf_info.items():
-            for dataset, varname in info.items():
-                start_time = time_range[0]
-                end_time = time_range[1]
-                while start_time < end_time:
-                    chunk_end_time = min(start_time + time_chunk, end_time)
-                    file_info_list.append((dataset, varname, start_time, chunk_end_time, save_path[satelite]))
-                    start_time = chunk_end_time
-                    
-    try:
-        asyncio.run(_fetch_files(file_info_list))
-    except Exception as e:
-        print(f'Error occured while fetching files: {str(e)}')
+    for satelite, info in cdf_info.items():
+        for dataset, varname in info.items():
+            start_time = time_range[0]
+            end_time = time_range[1]
+            while start_time < end_time:
+                chunk_end_time = min(start_time + time_chunk, end_time)
+                file_info_list.append((dataset, varname, start_time, chunk_end_time, save_path[satelite]))
+                start_time = chunk_end_time
+             
+    semaphore = asyncio.Semaphore(max_concurrent)
+    asyncio.run(_fetch_files(semaphore, file_info_list, retries, delay))
+        
+    return file_info_list
         
         
     
