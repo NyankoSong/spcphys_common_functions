d909374e1a6ff55bf65f2a614565e2c87acf37c5
NyankoSong <2010220018@email.cugb.edu.cn>
2024-11-19 17:31:43 +0800

    modified:   src/spcphys_common_functions/utils/vdf_process.py 	modified:   tests/vdf_process_test.ipynb

--- src/spcphys_common_functions/utils/vdf_process.py
+++ src/spcphys_common_functions/utils/vdf_process.py
@@ -108,7 +108,7 @@ def vdf_griddata(vdf_vec: u.Quantity, vdf_value: u.Quantity, grid_size: int, dev
     
     if device == 'cpu':
         pdf = sinterp.griddata(vdf_vec.to_value(), vdf_value.to_value(), v_gridmesh_points.to_value(), method='linear', fill_value=0) * valid_vdf_value.unit
-        pdf_3d = pdf.reshape(grid_size, grid_size, grid_size)
+        pdf_3d = pdf.reshape(grid_size, grid_size, grid_size) / pdf.sum()
         
     else:
         raise ValueError('Currently only support cpu.')
@@ -142,7 +142,7 @@ def vdf_griddata_t(vdf_vec_t: u.Quantity, vdf_value_t: u.Quantity, grid_size: in
     if device not in ['cpu', 'gpu']:
         raise ValueError("Device must be either 'cpu' or 'gpu'.")
 
-    pdf_t = np.zeros((vdf_value_t.shape[0], grid_size, grid_size, grid_size)) * vdf_value_t.unit
+    pdf_t = np.zeros((vdf_value_t.shape[0], grid_size, grid_size, grid_size)) * u.dimensionless_unscaled
     v_grid_t = np.zeros((vdf_value_t.shape[0], 3, grid_size)) * vdf_vec_t.unit
     
     vdf_input_t = [(vdf_vec_t[i, :, :], vdf_value_t[i, :], grid_size) for i in range(vdf_value_t.shape[0])]
@@ -159,22 +159,80 @@ def vdf_griddata_t(vdf_vec_t: u.Quantity, vdf_value_t: u.Quantity, grid_size: in
     return pdf_t, v_grid_t
 
 
-@check_parameters
-def vdf_core(pdf_t: u.Quantity, v_grid_t: u.Quantity) -> u.Quantity:
-        
-    v_t_max = np.zeros((v_grid_t.shape[0], 3)) * v_grid_t.unit
+def vdf_core(pdf_t: u.Quantity|np.ndarray, v_grid_t: u.Quantity|np.ndarray):
+    
+    v_t_max = np.zeros((v_grid_t.shape[0], 3))
         
     for (i, n) in enumerate(range(3)):
         v_grid_index = [j for j in range(len(pdf_t.shape) - 1)]
         v_grid_index.remove(n)
         pdf_1d_t = np.sum(pdf_t, axis=(v_grid_index[0] + 1, v_grid_index[1] + 1))
         pdf_1d_t_max_index = np.argmax(pdf_1d_t, axis=1)
-        v_t_max[:, i] = np.array([v_grid_t[j, n, pdf_1d_t_max_index[j]] for j in range(v_grid_t.shape[0])]).reshape(-1) * v_grid_t.unit
+        v_t_max[:, i] = np.array([v_grid_t[j, n, pdf_1d_t_max_index[j]] for j in range(v_grid_t.shape[0])]).reshape(-1)
         
-    return v_t_max
+    return v_t_max if isinstance(v_grid_t, np.ndarray) else v_t_max * v_grid_t.unit
+
+
+# 如果坐标系单位矢量z的N分量（e_z[-1]）小于0，那么对磁场的y分量取反，理由未知，逻辑来自Fortran程序
+def _plot_arrow(axes: plt.Axes, mag_vector_3d: np.ndarray, point_3d: np.ndarray, v_units: np.ndarray):
+    
+    v_unit_index = [0, 1]
+    mag_vector = np.zeros(2)
+    mag_vector[0] = np.dot(mag_vector_3d, v_units[v_unit_index[0]]) / np.linalg.norm(v_units[v_unit_index[0]])
+    mag_vector[1] = np.dot(mag_vector_3d, v_units[v_unit_index[1]]) / np.linalg.norm(v_units[v_unit_index[1]])
+    point = point_3d[v_unit_index]
+    
+    # 未知取反逻辑部分（存疑）：
+    if v_units[2, -1] < 0:
+        mag_vector[1] = -mag_vector[1]
+    
+    x_lim = axes.get_xlim()
+    y_lim = axes.get_ylim()
+    
+    x_width = x_lim[1] - x_lim[0]
+    y_height = y_lim[1] - y_lim[0]
+    scale_factor = 0.1
+    
+    x_lim = [x_lim[0] + x_width * scale_factor, x_lim[1] - x_width * scale_factor]
+    y_lim = [y_lim[0] + y_height * scale_factor, y_lim[1] - y_height * scale_factor]
+    
+    arrow_k = mag_vector[-1] / mag_vector[0]
+    # y - y0 = k * (x - x0)
+    y_0 = (x_lim[0] - point[0]) * arrow_k + point[1]
+    if y_0 > y_lim[1]:
+        y_0 = y_lim[1]
+        x_0 = (y_lim[1] - point[1]) / arrow_k + point[0]
+    elif y_0 < y_lim[0]:
+        y_0 = y_lim[0]
+        x_0 = (y_lim[0] - point[1]) / arrow_k + point[0]
+    else:
+        x_0 = x_lim[0]
+    
+    y_1 = (x_lim[1] - point[0]) * arrow_k + point[1]
+    if y_1 > y_lim[1]:
+        y_1 = y_lim[1]
+        x_1 = (y_lim[1] - point[1]) / arrow_k + point[0]
+    elif y_1 < y_lim[0]:
+        y_1 = y_lim[0]
+        x_1 = (y_lim[0] - point[1]) / arrow_k + point[0]
+    else:
+        x_1 = x_lim[1]
+    
+    head_size = (x_lim[1] - x_lim[0])/30
+    
+    if mag_vector[0] > 0:
+        axes.arrow(x_0, y_0, x_1 - x_0, y_1 - y_0, head_width=head_size, head_length=head_size*1.2, shape="full", fc='w', ec='w', lw=2)
+        axes.arrow(x_0, y_0, x_1 - x_0, y_1 - y_0, head_width=head_size, head_length=head_size, shape="full", fc='k', ec='k', lw=1)
+    else:
+        axes.arrow(x_1, y_1, x_0 - x_1, y_0 - y_1, head_width=head_size, head_length=head_size*1.2, shape="full", fc='w', ec='w', lw=2)
+        axes.arrow(x_1, y_1, x_0 - x_1, y_0 - y_1, head_width=head_size, head_length=head_size, shape="full", fc='k', ec='k', lw=1)
+
 
 @check_parameters
-def plot_vdf_2d(axes: plt.Axes, pdf: u.Quantity, v_grid: u.Quantity, compress_v_unit=2, color_norm: Normalize|LogNorm|None =None, color_levels: List[float]|None =None, cax: plt.Axes|None =None) -> QuadMesh:
+def plot_vdf_2d(axes: plt.Axes, pdf: u.Quantity|np.ndarray, v_grid: u.Quantity, compress_v_unit: int =2,
+                core_marker: bool|dict =False, imf_vector: u.Quantity|None =None, v_unit: np.ndarray|None =None,
+                clip_lower_percentage: float =0, color_norm: Normalize|LogNorm|None =None, color_levels: List[float]|None =None, cax: plt.Axes|None =None,
+                pcolormesh_kwargs: dict|None =None, contour_kwargs: bool|dict =True) -> QuadMesh:
     
     '''
     Plot the 2D VDF.
@@ -182,33 +240,63 @@ def plot_vdf_2d(axes: plt.Axes, pdf: u.Quantity, v_grid: u.Quantity, compress_v_
     :param axes: The axis to plot.
     :param pdf: The 3D grid of VDF in shape (grid_size, grid_size, grid_size).
     :param v_grid: The 3D grid of velocity in shape (3, grid_size).
-    :param target_v_unit: The unit of velocity that will not be plotted. Default is 2, which means the z-axis.
+    :param compress_v_unit: The unit of velocity that will not be plotted. Default is 2, which means the z-axis.
+    :param core_marker: Whether to plot the core marker. Could be a dictionary of the marker properties. Default is False.
+    :param imf_vector: Whether to plot the IMF vector. Default is None.
+    :param v_units: The base vectors of the new coordinate system. This must be provided if `imf_vector` is not None. Default is None.
+    :param clip_lower_percentage: The percentage of the lower limit of the color normalization. Should be between 0 and 1. Default is 0.
     :param color_norm: The color normalization. Default is None, which means the LogNorm will be used.
     :param color_levels: The color levels. Default is None, which means the levels will be calculated automatically.
+    :param cax: The color bar axis. Default is None.
+    :param pcolormesh_kwargs: The keyword arguments for the pcolormesh function. Default is None, which means {'cmap': 'jet', 'shading': 'auto'}.
+    :param contour_kwargs: The keyword arguments for the contour function. Default is True, which means {'colors':'k'}.
     
-    :return: The figure and axis.
+    :return quadmesh: The quadmesh of the plot.
     '''
     
+    if compress_v_unit not in [0, 1, 2]:
+        raise ValueError("compress_v_unit must be 0, 1, or 2.")
+    
+    if isinstance(pdf, np.ndarray):
+        pdf = pdf * u.dimensionless_unscaled
+    if pcolormesh_kwargs is None:
+        pcolormesh_kwargs = {'cmap': 'jet', 'shading': 'auto'}
+    if contour_kwargs is True:
+        contour_kwargs = {'colors':'k'}
     
     v_grid_index = [i for i in range(len(pdf.shape))]
     v_grid_index.remove(compress_v_unit)
     
     v_grid = v_grid.to(u.km/u.s).to_value()
-    pdf_2D = pdf.sum(axis=compress_v_unit).to_value()
+    pdf_2d = pdf.sum(axis=compress_v_unit).to_value()
     
     if color_norm is None:
-        low_level = np.log10(np.min(pdf_2D[pdf_2D > 0])) + (np.log10(np.max(pdf_2D)) - np.log10(np.min(pdf_2D[pdf_2D > 0])))/2
-        high_level = np.log10(np.max(pdf_2D))
+        low_level = np.log10(np.min(pdf_2d[pdf_2d > 0])) + (np.log10(np.max(pdf_2d)) - np.log10(np.min(pdf_2d[pdf_2d > 0]))) * clip_lower_percentage
+        high_level = np.log10(np.max(pdf_2d))
         color_norm = LogNorm(vmin=10**low_level, vmax=10**high_level)
     if color_levels is None and isinstance(color_norm, LogNorm):
+        low_level = np.log10(color_norm.vmin)
+        high_level = np.log10(color_norm.vmax)
         color_levels = np.logspace(int(low_level), int(high_level), (int(high_level) + 1 - int(low_level)))
     
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=UserWarning)
 
-        quadmesh = axes.pcolormesh(v_grid[v_grid_index[0]], v_grid[v_grid_index[1]], pdf_2D.T, cmap='jet', norm=color_norm, shading='auto')
-        if color_levels is not None:
-            axes.contour(v_grid[v_grid_index[0]], v_grid[v_grid_index[1]], pdf_2D.T, colors='k', norm=color_norm, levels=color_levels)
+        quadmesh = axes.pcolormesh(v_grid[v_grid_index[0]], v_grid[v_grid_index[1]], pdf_2d.T, norm=color_norm, **pcolormesh_kwargs)
+        if color_levels is not None and contour_kwargs is not None:
+            contour_kwargs['levels'] = color_levels
+            axes.contour(v_grid[v_grid_index[0]], v_grid[v_grid_index[1]], pdf_2d.T, norm=color_norm, **contour_kwargs)
+            
+    core_pos = vdf_core(pdf[np.newaxis, :], v_grid[np.newaxis, :])[0]
+    if core_marker is not False:
+        index = [i for i in range(3) if i != compress_v_unit]
+        if core_marker is True:
+            axes.plot(core_pos[index[0]], core_pos[index[1]], 'kx', markersize=10)
+        elif isinstance(core_marker, dict):
+            axes.plot(core_pos[index[0]], core_pos[index[1]], **core_marker)
+            
+    if imf_vector is not None and v_unit is not None:
+        _plot_arrow(axes, imf_vector.to_value(), core_pos, v_unit)
         
     axes.grid('both', alpha=0.5)
     
@@ -221,7 +309,10 @@ def plot_vdf_2d(axes: plt.Axes, pdf: u.Quantity, v_grid: u.Quantity, compress_v_
     return quadmesh
 
 
-def plot_vdf_1d_t(axes: plt.Axes, time: np.ndarray|List[datetime], pdf_t: u.Quantity, v_grid_t: u.Quantity, target_v_unit: int =0, color_norm: Normalize|LogNorm|None =None, core_line: str|None =None, cax: plt.Axes|None =None) -> QuadMesh:
+@check_parameters
+def plot_vdf_1d_t(axes: plt.Axes, time: np.ndarray|List[datetime], pdf_t: u.Quantity|np.ndarray, v_grid_t: u.Quantity, target_v_unit: int =0, 
+                  clip_lower_percentage: float =0, color_norm: Normalize|LogNorm|None =None, core_line: bool|dict =False, cax: plt.Axes|None =None, 
+                  pcolormesh_kwargs: dict|None =None) -> QuadMesh:
     
     '''
     Plot 1D VDF time series.
@@ -231,23 +322,30 @@ def plot_vdf_1d_t(axes: plt.Axes, time: np.ndarray|List[datetime], pdf_t: u.Quan
     :param pdf_t: The 3D grid of VDF in shape (time, grid_size, grid_size, grid_size).
     :param v_grid_t: The 3D grid of velocity in shape (time, 3, grid_size).
     :param target_v_unit: The unit of velocity that will be plotted. Default is 0, which means the x-axis.
+    :param clip_lower_percentage: The percentage of the lower limit of the color normalization. Should be between 0 and 1. Default is 0.
     :param color_norm: The color normalization. Default is None, which means the LogNorm will be used.
-    :param core_line: The line style of the core line. Default is None.
+    :param core_line: Whether to plot the core line. Could be a dictionary of the line properties. Default is False.
     :param cax: The color bar axis. Default is None.
+    :param pcolormesh_kwargs: The keyword arguments for the pcolormesh function. Default is None, which means {'cmap': 'jet', 'shading': 'auto'}.
     
-    :return: The figure and axis.
+    :return quadmesh: The quadmesh of the plot.
     '''
+    
+    if isinstance(pdf_t, np.ndarray):
+        pdf_t = pdf_t * u.dimensionless_unscaled
+    if pcolormesh_kwargs is None:
+        pcolormesh_kwargs = {'cmap': 'jet', 'shading': 'auto'}
         
     v_grid_index = [i for i in range(len(pdf_t.shape) - 1)]
     v_grid_index.remove(target_v_unit)
     
     v_grid_t = v_grid_t.to(u.km/u.s).to_value()
-    pdf_1D_t = np.sum(pdf_t, axis=(v_grid_index[0] + 1, v_grid_index[1] + 1)).to_value()
-    pdf_1D_t_max_index = np.argmax(pdf_1D_t, axis=1)
+    pdf_1d_t = np.sum(pdf_t, axis=(v_grid_index[0] + 1, v_grid_index[1] + 1)).to_value()
+    pdf_1d_t_max_index = np.argmax(pdf_1d_t, axis=1)
     
     if color_norm is None:
-        low_level = np.log10(np.min(pdf_1D_t[pdf_1D_t > 0])) + (np.log10(np.max(pdf_1D_t)) - np.log10(np.min(pdf_1D_t[pdf_1D_t > 0])))/2
-        high_level = np.log10(np.max(pdf_1D_t))
+        low_level = np.log10(np.min(pdf_1d_t[pdf_1d_t > 0])) + (np.log10(np.max(pdf_1d_t)) - np.log10(np.min(pdf_1d_t[pdf_1d_t > 0]))) * clip_lower_percentage
+        high_level = np.log10(np.max(pdf_1d_t))
         color_levels = np.logspace(int(low_level), int(high_level), (int(high_level) + 1 - int(low_level)))
         color_norm = LogNorm(vmin=10**low_level, vmax=10**high_level)
     else:
@@ -255,11 +353,14 @@ def plot_vdf_1d_t(axes: plt.Axes, time: np.ndarray|List[datetime], pdf_t: u.Quan
         
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=UserWarning)
-        for i in range(pdf_1D_t.shape[0]):
-            quadmesh = axes.pcolormesh([time[i], time[i] + (time[i+1] - time[i])/2 if i < pdf_1D_t.shape[0] - 1 else time[i] + (time[i] - time[i-1])/2], v_grid_t[i, target_v_unit, :], pdf_1D_t[[i, i], :].T, cmap='jet', norm=color_norm, shading='auto')
+        for i in range(pdf_1d_t.shape[0]):
+            quadmesh = axes.pcolormesh([time[i], time[i] + (time[i+1] - time[i])/2 if i < pdf_1d_t.shape[0] - 1 else time[i] + (time[i] - time[i-1])/2], v_grid_t[i, target_v_unit, :], pdf_1d_t[[i, i], :].T, norm=color_norm, **pcolormesh_kwargs)
 
-        if core_line is not None:
-            axes.plot(time, np.array([v_grid_t[i, target_v_unit, pdf_1D_t_max_index[i]] for i in range(len(time))]).reshape(-1), core_line, linewidth=1)
+        if core_line is True:
+            axes.plot(time, np.array([v_grid_t[i, target_v_unit, pdf_1d_t_max_index[i]] for i in range(len(time))]).reshape(-1), 'w-', linewidth=2)
+            axes.plot(time, np.array([v_grid_t[i, target_v_unit, pdf_1d_t_max_index[i]] for i in range(len(time))]).reshape(-1), 'k-', linewidth=1)
+        elif isinstance(core_line, dict):
+            axes.plot(time, np.array([v_grid_t[i, target_v_unit, pdf_1d_t_max_index[i]] for i in range(len(time))]).reshape(-1), **core_line)
         
     axes.grid('both', alpha=0.5)
     
